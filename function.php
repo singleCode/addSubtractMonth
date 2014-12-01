256e993cbfc8e0b0b4d6f9234a8eeb90769478c0
singleCode <geezzakh@gmail.com>
2014-12-01 10:11:40 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,31 +1,32 @@
-<?php
 class tweakMonth{
-	public function addSubtractMonth($date,$number){
-		$date = new DateTime($date);
-#explode date
-		list($y, $m, $d) = explode('-', $date->format('Y-m-d'));
-		if($number > 0){
-			$m += $number;
-			while ($m > 12) {
-				$m -= 12;
-				$y++;
+		public function addSubtractMonth($din,$number){
+			$date = new DateTime($din);
+			#explode date
+			list($y, $m, $d) = explode('-', $date->format('Y-m-d'));
+
+			if($number > 0){
+				$m += $number;
+				while ($m > 12) {
+					$m -= 12;
+					$y++;
+				}
+				!dd($m);
+			}else{
+
+				$m +=$number;
+				while ($m < 1) {
+					$m += 12;
+					$y--;
+				}
 			}
-		}else{
-			$m -=$number;
-			while ($m < 1) {
-				$m += 12;
-				$y--;
+			$last = date('t', strtotime("$y-$m-1"));
+			if($din =$last or $last < $d){
+				$day = $last;
+			}else{
+				$day = $d;
 			}
+		
+			$date->setDate($y, $m, $day);
+			return $date->format('Y-m-d');
 		}
-		$last = date('t', strtotime("$y-$m-1"));
-		if($l=='1' or $last < $d){
-			$day = $last;
-		}else{
-			$day = $d;
-		}
-#Set tanggal setelah modifikasi
-		$date->setDate($y, $m, $day);
-		return $date->format('Y-m-d');
 	}
-}
-?>
